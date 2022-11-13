6f142f0c6a02dedcf3e8159727c83334dab59726
Arbaz ali <30619186+Arbaxali@users.noreply.github.com>
2022-11-13 22:58:59 +0530

    update

--- final project/evrythingblog.py
+++ final project/evrythingblog.py
@@ -1,47 +1,57 @@
-# from urllib import response
-# from flask import Flask, render_template, request, redirect
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, SubmitField
-# from wtforms.validators import DataRequired 
-# import requests
-
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = "hello"
-
-# form class
-# class NamerForm(FlaskForm):
-#     idname = StringField("whats your name", validators=[DataRequired("name")])
-#     idpass = StringField("enter pass", validators=[DataRequired("pass")])
-#     Submit = SubmitField("Submit")
-
-# @app.route('/name', methods =['GET','POST'])
-
-# def name():
-#     idname = None
-#     idpass = None
-#     form = NamerForm()
-#     if form.validate_on_submit():
-#         idname = form.idname.data
-#         idpass = form.idpass.data
-#         form.idname.data = ''
-#         form.idpass.data = ''
-#     return render_template("index1.html",idname = idname, idpass = idpass,form = form)
-
 # importing Flask and other modules
-from flask import Flask, request, render_template
-
+from flask import Flask, request, render_template ,redirect
+import pymongo
+import datetime
 # Flask constructor
 app = Flask(__name__)
 
+try:
+    client = pymongo.MongoClient("mongodb://localhost:27017")
+    client.server_info()
+    db = client['blogsarticles']
+    collection = db['blogsCollection']
+    print(client)
+    
+except:
+    print("cannot connect to mongodb")        
+
 # A decorator used to tell the application
 # which URL is associated function
-@app.route('/', methods =["GET", "POST"])
+@app.route('/login', methods =["GET", "POST"])
 def gfg():
     if request.method == "POST":
         id_name = request.form.get("fname")
         password = request.form.get("lname")
-        return "Your name is "+id_name+ password
-    return render_template("index1.html")
+        return redirect("display")
+    return render_template("login.html")
+
+
+@app.route('/signup', methods =["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        id_name = request.form.get("fname")
+        password = request.form.get("lname")
+        return redirect("login")
+    return render_template("signup.html")    
+
+@app.route('/show', methods =["GET", "POST"])
+def show():
+    if request.method == "POST":
+        document = {
+            'name': request.form.get("names"),
+            'title': request.form.get("titles"),
+            'article': request.form.get("textareas"),
+            'last_modified': datetime.datetime.now()
+        } 
+        collection.insert_one(document)
+        return redirect("display")   
+    return render_template('show.html')
+
+@app.route('/display', methods =["GET", "POST"])
+def display():
+    # if request.method == "POST":
+    allproducts =collection.find({})
+    return render_template('display.html', allproducts = allproducts)
 
 if __name__=='__main__':
     app.run(host = "0.0.0.0", port=70, debug=True)
